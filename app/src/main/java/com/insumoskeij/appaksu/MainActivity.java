80d3351a9744ab3d2e3448776b509b201e1f1390
Erick J <erickjac18@gmail.com>
2018-12-05 16:44:11 -0400

    boton nueva busqueda

--- app/src/main/java/com/insumoskeij/appaksu/MainActivity.java
+++ app/src/main/java/com/insumoskeij/appaksu/MainActivity.java
@@ -147,6 +147,19 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        FloatingActionButton limpiar = findViewById(R.id.fabClean);
+        limpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
